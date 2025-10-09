48e053bcc4fd67a9ddc60040eb7510d5fe3db823
Jayesh Parab <jrparab2000@gmail.com>
2025-10-09 13:59:24 -0500

    commit 11 (Virtual env)

--- Vitual_env.py
+++ Vitual_env.py
@@ -1,10 +1,10 @@
 # create a python environment vitually in command promt
 # this is the command "python -m venv myenv"
-# -m    :   it calls the module 
-# venv  :   means create a virtual environment 
-# myenv :   is the name of the environment 
+# -m    :   it calls the module
+# venv  :   means create a virtual environment
+# myenv :   is the name of the environment
 
-# using library and using it using .venv in vs code 
+# using library and using it using myenv in vs code
 
 # use this command to activate ./Scripts/activate
 
@@ -12,15 +12,19 @@
 # Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 
-import matplotlib.pyplot as plt
-import plotly.express
-import pandas
+# import matplotlib.pyplot as plt
+# import plotly.express
+# import pandas
 
 
-x = [1,2,3,4,5]
-y = [2,4,6,8,10]
+# x = [1,2,3,4,5]
+# y = [2,4,6,8,10]
 
-fig = plotly.express.scatter(x,y)
-fig.show()
-plt.plot(x)
-plt.show()
+# fig = plotly.express.scatter(x,y)
+# fig.show()
+# plt.plot(x)
+# plt.show()
+
+lists_of_names = ["Jayesh" "Shruti" "Alice" "Bob"]
+for name in lists_of_names:
+    print(name)
