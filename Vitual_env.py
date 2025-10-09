a856f9e18019e8ada277aa27efc811a581463a9e
Jayesh Parab <jrparab2000@gmail.com>
2025-10-09 12:12:54 -0500

    commit virtual env

---
+++ Vitual_env.py
@@ -0,0 +1,5 @@
+# create a python environment vitually in command promt
+# this is the command "python -m venv myenv"
+# -m    :   it calls the module 
+# venv  :   means create a virtual environment 
+# myenv :   is the name of the environment 
